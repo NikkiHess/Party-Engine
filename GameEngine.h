8322f540d4b4334192cdc0a18e73a4f2ff7e5c3f
Nikki <coinslotgaming@gmail.com>
2024-01-28 20:11:15 -0500

    please grade homework3 windows

--- GameEngine.h
+++ GameEngine.h
@@ -10,20 +10,24 @@ const int NUM_HARDCODED_ACTORS = 14;
 
 class Engine {
 public:
+	enum GameState {
+		NORMAL, WIN, LOSE
+	};
+
 	bool game_running = false; // is the game running? drives the start loop
 	char render_map[HARDCODED_MAP_HEIGHT][HARDCODED_MAP_WIDTH + 1]; // the map to be rendered - updated each frame
 
 	// player stuff
 	glm::ivec2 updated_player_pos; // the "future" player position
-	unsigned int player_health = 3; // the player's current health
-	unsigned int player_score = 0; // the player's current score
+	int player_health = 3; // the player's current health
+	int player_score = 0; // the player's current score
 
 	// actor stuff
 	std::map<Actor*, bool> triggered_score_up; // keep track of which actors triggered a player score up
 
 	void start(); // starts the engine
 	void stop(); // stops the engine
-	void render(); // render the current view
+	GameState render(); // render the current view, returns the current GameState
 	void show_stats(); // show the player's statistics
 	void prompt_player(); // prompt the player for input
 	void update_positions(); // update all actor positions
@@ -32,5 +36,5 @@ public:
 private:
 	glm::ivec2 renderSize;
 
-	void execute_commands(Actor& trigger, std::string& dialogue);
+	GameState execute_commands(Actor& trigger, std::string& dialogue);
 };
