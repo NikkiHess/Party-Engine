e89950fb58d192da4284be60c6667935f97a8f3a
Nikki <coinslotgaming@gmail.com>
2024-02-04 04:12:19 -0500

    Fixed erroneous GetStringOrDefault() in ConfigUtils - please grade homework4 windows

--- src/utils/ConfigUtils.cpp
+++ src/utils/ConfigUtils.cpp
@@ -113,7 +113,7 @@ void ConfigUtils::initializeScene(Scene &scene, rapidjson::Document& document, b
 
 void ConfigUtils::setActorProps(ActorProps& props, rapidjson::Value& document) {
 	if (document.HasMember("name"))
-		props.name = document["name"].GetStringOrDefault();
+		props.name = document["name"].GetString();
 	if (document.HasMember("view"))
 		props.view = document["view"].GetString()[0];
 	if (document.HasMember("x"))
