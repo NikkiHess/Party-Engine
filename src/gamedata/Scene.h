4aa67354bac01770bdb3e9e583a3f5c895ce4a58
Nikki <coinslotgaming@gmail.com>
2024-02-05 15:01:30 -0500

    please grade homework4 windows please grade homework4 linux
    Can I do multiple please grades at once?

--- src/gamedata/Scene.h
+++ src/gamedata/Scene.h
@@ -10,22 +10,25 @@
 
 // dependencies
 #include "glm/glm.hpp"
+#define GLM_ENABLE_EXPERIMENTAL
+#include "glm/gtx/hash.hpp"
 
 class Scene
 {
-private:
-	struct KeyFuncs {
-		size_t operator()(const glm::ivec2& k) const {
-			return std::hash<int>()(k.x) ^ std::hash<int>()(k.y);
-		}
-
-		bool operator()(const glm::ivec2& a, const glm::ivec2& b) const {
-			return a.x == b.x && a.y == b.y;
-		}
-	};
+//private:
+//	struct KeyFuncs {
+//		size_t operator()(const glm::ivec2& k) const {
+//			return std::hash<int>()(k.x) ^ std::hash<int>()(k.y);
+//		}
+//
+//		bool operator()(const glm::ivec2& a, const glm::ivec2& b) const {
+//			return a.x == b.x && a.y == b.y;
+//		}
+//	};
 public:
 	std::vector<Actor> actors;
-	std::unordered_map<glm::ivec2, std::vector<Actor*>, KeyFuncs, KeyFuncs> locToActors;
+	//std::unordered_map<glm::ivec2, std::vector<Actor*>, KeyFuncs, KeyFuncs> locToActors;
+	std::unordered_map<glm::ivec2, std::vector<Actor*>> locToActors;
 	std::string name;
 
 	// instantiate an actor in the scene, returns the actor
