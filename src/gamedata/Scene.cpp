744908fd1772afafb2fc53c0b8b5486a2bd44122
Nikki <coinslotgaming@gmail.com>
2024-02-11 06:13:41 -0500

    Revert "Swap source of actorPtr (please don't break)"
    This reverts commit 10ad811cb8f3c0c8b2bcc8a24de44cad559efbfc.

--- src/gamedata/Scene.cpp
+++ src/gamedata/Scene.cpp
@@ -9,8 +9,8 @@
 
 Actor& Scene::instantiateActor(ActorProps& props) {
 	glm::dvec2 actorPos(props.transform.pos.x, props.transform.pos.y);
-	Actor actor = Actor(props);
-	Actor* actorPtr = &actor;
+	Actor* actorPtr = new Actor(props);
+	Actor &actor = *actorPtr;
 	actor.id = actors.size();
 	
 	// insert the actor into the list of actors
