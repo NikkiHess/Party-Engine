ecf3b48da89e02dde907acfa5dd899860e9043f8
Nikki <coinslotgaming@gmail.com>
2024-02-19 05:09:02 -0500

    :pencil2: Uncomment audio code
    please grade homewok6 linux

--- src/audio/AudioPlayer.cpp
+++ src/audio/AudioPlayer.cpp
@@ -10,32 +10,32 @@
 #include "AudioHelper.h"
 
 void AudioPlayer::play(std::string& soundName, int loops) {
-	//std::string soundPath = "resources/audio/" + soundName; // the audio's path
-	//// Verify that the intro music exists
-	//if (resourceManager.fileExists(soundPath + ".wav")) {
-	//	soundPath += ".wav";
-	//}
-	//else if (resourceManager.fileExists(soundPath + ".ogg")) {
-	//	soundPath += ".ogg";
-	//}
-	//if (soundPath == "resources/audio/" + soundName) {
-	//	Error::error("failed to play audio clip " + soundName);
-	//}
+	std::string soundPath = "resources/audio/" + soundName; // the audio's path
+	// Verify that the intro music exists
+	if (resourceManager.fileExists(soundPath + ".wav")) {
+		soundPath += ".wav";
+	}
+	else if (resourceManager.fileExists(soundPath + ".ogg")) {
+		soundPath += ".ogg";
+	}
+	if (soundPath == "resources/audio/" + soundName) {
+		Error::error("failed to play audio clip " + soundName);
+	}
 
-	//// the sound to be played
-	//Mix_Chunk* sound = nullptr;
+	// the sound to be played
+	Mix_Chunk* sound = nullptr;
 
-	//// Is the sound cached? Load it
-	//auto it = resourceManager.sounds.find(soundName);
-	//if (it != resourceManager.sounds.end()) {
-	//	sound = it->second;
-	//}
-	//// Otherwise, load from path and cache it
-	//else {
-	//	sound = AudioHelper::Mix_LoadWAV498(soundPath.c_str());
-	//	resourceManager.sounds[soundName] = sound;
-	//}
+	// Is the sound cached? Load it
+	auto it = resourceManager.sounds.find(soundName);
+	if (it != resourceManager.sounds.end()) {
+		sound = it->second;
+	}
+	// Otherwise, load from path and cache it
+	else {
+		sound = AudioHelper::Mix_LoadWAV498(soundPath.c_str());
+		resourceManager.sounds[soundName] = sound;
+	}
 
-	//// Play the sound on channel 0, looping indefinitely
-	//AudioHelper::Mix_PlayChannel498(0, sound, loops);
+	// Play the sound on channel 0, looping indefinitely
+	AudioHelper::Mix_PlayChannel498(0, sound, loops);
 }
